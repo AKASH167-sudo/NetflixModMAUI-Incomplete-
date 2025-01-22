ddda19b8db788b9515c5893048ad31ea7ba2eae3
AKASH KUMAR <993284vks@gmail.com>
2025-01-22 18:56:56 +0530

    Add files via upload

---
+++ Services/TmdbService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixCloneMAUI.Services
+{
+    public class TmdbService
+    {
+        private const string ApiKey = "1311c1986571c466b5ebe41df41bd527";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TmdbService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private HttpClient HttpClient => _httpClientFactory.CreateClient();
+    }
+}
